bede5e1c114cd0d738671481c564a0e24298debe
Zachary Thomas <111013875+ZacRayTho@users.noreply.github.com>
2023-08-15 19:32:47 -0400

    Update ValidSudoku.py

--- LeetCode/Python/ValidSudoku.py
+++ LeetCode/Python/ValidSudoku.py
@@ -4,6 +4,27 @@ class Solution:
         row = collections.defaultdict(set)
         square = collections.defaultdict(set)
         
-        for i in board:
-            for j in i:
-                print(j)
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+
+                if board[i][j] in row[i]:
+                    print(row[i])
+                    print("row")
+                    return False
+
+                if board[i][j] in column[j]:
+                    print("col")
+                    return False
+
+                if board[i][j] in square[(i // 3, j // 3)]:
+                    print("square")
+                    return False
+
+                column[j].add(board[i][j])
+                row[i].add(board[i][j])
+                square[(i // 3, j // 3)].add(board[i][j])
+
+        return True
+                
